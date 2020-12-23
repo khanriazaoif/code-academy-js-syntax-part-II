9300519bff17a202b86878306c8e933386008102
Riaz Khan <khanriazaoif@gmail.com>
2020-12-23 11:17:08 -0600

    updaing-initial-input-whale-talk

--- whale-talk.js
+++ whale-talk.js
@@ -1,28 +1,19 @@
-let input = `Riaz is awesome, he is just the coolest.`;
+let input = `Learning to code is fun.`;
 
 const vowels = ['a', 'e', 'i', 'o', 'u'];
 
 let resultArray = [];
 
 for (let i = 0; i < input.length; i++) {
-    // console.log(input([i]));
     let inputIndex = input[i];
-    // console.log(inputIndex);
-    // console.log(inputIndex[i]);
     for (let j = 0; j < vowels.length; j++){
-        // console.log([j]);
         let vowelsIndex = vowels[j];
-        // console.log(inputIndex);
-        // console.log(vowelsIndex);
         if(inputIndex === vowelsIndex){
             resultArray.push(vowelsIndex);
-            // console.log(inputIndex);
             if(inputIndex === 'e' || inputIndex === 'u'){
                 resultArray.push(inputIndex);
-                // console.log(resultArray);
             }
         }
-        // resultArray.push(vowelsIndex);
     }
 }
 
