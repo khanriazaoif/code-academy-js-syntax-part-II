077749d7e68eefd49a6512eb9421b3869db42e39
Riaz Khan <khanriazaoif@gmail.com>
2020-12-22 23:35:17 -0600

    test-2

--- secret-message.js
+++ secret-message.js
@@ -1,4 +1,5 @@
 let secretMessage = ['Learning', 'is', 'not', 'about', 'what', 'you', 'get', 'easily', 'the', 'first', 'time,', 'it', 'is', 'about', 'what', 'you', 'can', 'figure', 'out.', '-2015,', 'Chris', 'Pine,', 'Learn', 'JavaScript'];
 
+
 console.log(secretMessage.pop());
-console.log(secretMessage);
+console.log(secretMessage.length);
