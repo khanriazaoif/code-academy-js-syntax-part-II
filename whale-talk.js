70abaf08dbf1929587badb0cc938184a669409fb
Riaz Khan <khanriazaoif@gmail.com>
2020-12-23 00:16:08 -0600

    task-5-whale-talk

--- whale-talk.js
+++ whale-talk.js
@@ -3,3 +3,7 @@ let input = 'Riaz is going to be a developer, believe that.';
 const vowels = ['a', 'e', 'i', 'o', 'u'];
 
 let resultArray = [];
+
+for (let i = 0; i < input.length; i++) {
+    console.log([i]);
+}
