44ad732783e2696858f55fd7e1d7075e710aa84b
Riaz Khan <khanriazaoif@gmail.com>
2020-12-23 15:51:32 -0600

    task-4-meal-maker-3
    I’m experimenting with a test-branch and master branch on my git.

--- meal-maker.js
+++ meal-maker.js
@@ -3,5 +3,25 @@ const menu = {
         appetizers: [],
         mains: [],
         desserts: [],
-    }
+        
+    },
+    get appetizers(){
+        return this._courses;
+    },
+    set appetizers(appetizerIn){
+        return this._courses;
+    },
+    get mains(){
+        return this._courses;
+    },
+    set mains(mainIn){
+        return this._courses
+    },
+    get desserts(){
+        return this._courses;
+    },
+    set desserts(dessertIn){
+        return this._courses
+    },
+
 };
