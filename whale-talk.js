68a0aeadbdf020bdec43d8e441e91245be68ba35
Riaz Khan <khanriazaoif@gmail.com>
2020-12-23 10:47:17 -0600

    task-8-whale-talk

--- whale-talk.js
+++ whale-talk.js
@@ -5,8 +5,22 @@ const vowels = ['a', 'e', 'i', 'o', 'u'];
 let resultArray = [];
 
 for (let i = 0; i < input.length; i++) {
-    console.log([i]);
+    // console.log(input([i]));
+    let inputIndex = input[i];
+    // console.log(inputIndex);
+    // console.log(inputIndex[i]);
     for (let j = 0; j < vowels.length; j++){
-        console.log([i]);
+        // console.log([j]);
+        let vowelsIndex = vowels[j];
+        console.log(inputIndex);
+        console.log(vowelsIndex);
+        if(inputIndex === vowelsIndex){
+            if(inputIndex === vowelsIndex){
+                resultArray.push(vowelsIndex);
+                console.log(resultArray);
+            }
+        }
+        
     }
 }
+
