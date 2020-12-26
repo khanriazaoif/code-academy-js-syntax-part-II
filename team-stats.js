2fb138851b0a64a00b00b721450c48e201b4c38d
Riaz Khan <khanriazaoif@gmail.com>
2020-12-26 10:10:43 -0600

    task-complete-team-stats-end-project
    The end of the team-stats project.

--- team-stats.js
+++ team-stats.js
@@ -40,13 +40,22 @@ const team = {
         return this._games;
     },
     addPlayer(firstName, lastName, age){
-        // debugger;
         let player = {
             firstName: firstName,
             lastName: lastName,
             age: age
         };
         this._players.push(player);
+    },
+
+    addGame(opponent, teamPoints, opponentPoints){
+        let game = {
+            opponent: opponent,
+            teamPoints: teamPoints,
+            opponentPoints: opponentPoints
+        }
+        this._games.push(game);
+
     }
 
 
@@ -56,5 +65,11 @@ team.addPlayer('Steph', 'Curry', 28);
 team.addPlayer('Lisa', 'Leslie', 44);
 team.addPlayer('Bugs', 'Bunny', 76);
 
+team.addGame('Jacksonville', 12, 28);
+team.addGame('Tampa', 32, 18);
+team.addGame('Miami', 21, 35);
+
+
 
 console.log(team._players);
+console.log(team._games);
