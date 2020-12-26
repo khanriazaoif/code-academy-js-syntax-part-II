fc08f4049bb7eea30cf97b43f7272433e8712717
Riaz Khan <khanriazaoif@gmail.com>
2020-12-26 09:06:10 -0600

    task-4-team-stats

--- team-stats.js
+++ team-stats.js
@@ -3,19 +3,35 @@ const team = {
         {
             firstName: 'Riaz',
             lastName: 'Khan',
-            age: 41,
+            age: 41
           },
           {
             firstName: 'Kamal',
             lastName: 'Xavier',
-            age: 11,
+            age: 11
           },
           {
             firstName: 'Zara',
             lastName: 'Isabella',
-            age: 7,
+            age: 7
           }
     ]
-    _games = []
+    _games = [
+        {
+            opponent: 'Cowboys',
+            teamPoints: 42,
+            opponentPoints: 17
+          },
+          {
+            opponent: 'Patriots',
+            teamPoints: 12,
+            opponentPoints: 27
+          },
+          {
+            opponent: 'Lions',
+            teamPoints: 32,
+            opponentPoints: 56
+          }
+    ]
 
 };
