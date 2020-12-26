ad20d1bc29ddc6a6375f05f575cfe92d6c45398d
Riaz Khan <khanriazaoif@gmail.com>
2020-12-26 09:03:52 -0600

    task-2-and-3-team-stats-project

--- team-stats.js
+++ team-stats.js
@@ -1,3 +1,21 @@
 const team = {
+    _players = [
+        {
+            firstName: 'Riaz',
+            lastName: 'Khan',
+            age: 41,
+          },
+          {
+            firstName: 'Kamal',
+            lastName: 'Xavier',
+            age: 11,
+          },
+          {
+            firstName: 'Zara',
+            lastName: 'Isabella',
+            age: 7,
+          }
+    ]
+    _games = []
 
 };
